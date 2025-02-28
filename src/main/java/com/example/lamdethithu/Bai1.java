c8daad27b458cf04347e9e4e529cc9381bcb806d
minhvu2005 <vu2691744@gmail.com>
2025-03-01 00:39:11 +0700

    Create maven.yml

--- src/main/java/com/example/lamdethithu/Bai1.java
+++ src/main/java/com/example/lamdethithu/Bai1.java
@@ -4,7 +4,7 @@ package com.example.lamdethithu;
 public class Bai1 {
     public int KiemThuTinhTong(int... numbers) {
         if (numbers.length == 0){
-         throw new IllegalArgumentException("Mang Khoong Duoc Trong");
+         throw new IllegalArgumentException("Mang Khoong Duoc ");
         }
         int total = 0;
         for (int num : numbers) {
